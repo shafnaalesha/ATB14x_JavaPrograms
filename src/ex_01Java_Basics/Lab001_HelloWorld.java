bec49f6877287ea10e29e73420ef9e55b311bfac
Shafna Shamsudeen <shafnaalesha@gmail.com>
2025-09-30 09:34:51 +0530

    Class files Literals23

--- src/ex_01Java_Basics/Lab001_HelloWorld.java
+++ src/ex_01Java_Basics/Lab001_HelloWorld.java
@@ -1,7 +1,8 @@
-package Java_Basics_ex_01;
+package ex_01Java_Basics;
 
 public class Lab001_HelloWorld {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello World");
     }
 }
